15a3fd3e59e8e8146f0e69083432f5e5365f65cd
David Dvořák <dvorakdavid7@gmail.com>
2024-12-03 12:17:04 +0100

    day 3 second part

--- 2024/day03/python/main.py
+++ 2024/day03/python/main.py
@@ -6,7 +6,7 @@ def mul(m: str):
     return n1 * n2
 
 
-def main():
+def part1():
     pattern = r"mul\(\d+,\d+\)"
     with open("input.txt", "r") as f:
         data = f.read()
@@ -26,6 +26,43 @@ def main():
     
     print(result)
 
+def part2():
+    mul_pattern = r"mul\(\d+,\d+\)"
+    do_pattern = r"do\(\)"
+    dont_pattern = r"don't\(\)"
+
+    with open("input.txt", "r") as f:
+        data = f.read()
+
+    buffer: str = ""
+
+    result = 0
+    enabled = True
+
+    for ch in data:
+        buffer += ch
+
+        mul_match = re.search(mul_pattern, buffer)
+        do_match = re.search(do_pattern, buffer)
+        dont_match = re.search(dont_pattern, buffer)
+
+        if dont_match:
+            enabled = False
+            buffer = ""
+
+        if do_match:
+            enabled = True
+            buffer = ""
+
+        if mul_match:
+            m = buffer[mul_match.start():mul_match.end()]
+            if enabled:
+                result += mul(m)
+            buffer = ""
+
+    print(result)
+
 
 if __name__ == "__main__":
-    main()
+    part1()
+    part2()
