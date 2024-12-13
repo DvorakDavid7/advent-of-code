f9962add32b88cb8035640d6650c09e4569059bb
David Dvořák <dvorakdavid7@gmail.com>
2024-12-13 17:25:47 +0100

    day 13

---
+++ 2024/day13/python/part2.py
@@ -0,0 +1,34 @@
+import re
+
+with open("input.txt", "r") as f:
+    data = f.read().splitlines()
+
+def parse_line(line):
+    x, y = line.split(",")
+    return int("".join(re.findall(r"\d+", x))), int("".join(re.findall(r"\d+", y)))
+
+def solve(a, b, c, d, u, v) -> tuple[int, int]:
+    if a == 0 or (a*d - c*b == 0):
+        raise ValueError("possible division by zero")
+    y = (a*v - c*u) / (a*d - c*b)
+    x = (u - b*y) / a
+    if not x.is_integer() or not y.is_integer():
+        raise ValueError("not an integer solution")
+    return int(x), int(y)
+
+result = 0
+constant = 10000000000000
+while data:
+    buff = data[:3]
+    data = data[4:]
+    ax, ay = parse_line(buff[0])
+    bx, by = parse_line(buff[1])
+    px, py = parse_line(buff[2])
+    try:
+        a, b = solve(ax, bx, ay, by, px + constant, py + constant)
+        result += a * 3 + b * 1
+    except:
+        pass
+
+print(result)
+
