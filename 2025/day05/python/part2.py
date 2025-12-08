2193b09e908a76fff8948452495202db704c6891
DVOŘÁK David <dvorakdavid7@gmail.com>
2025-12-08 11:04:10 +0100

    day 05 part 2

--- 2025/day05/python/part2.py
+++ 2025/day05/python/part2.py
@@ -1,29 +1,52 @@
 
+Interval = tuple[int, int]
+
 def parse_interval(x:str):
     l, r = x.split("-")
     return (int(l), int(r))
 
-def is_in_intervals(x: int, intervals: list[tuple[int, int]]) -> bool:
-    for i in intervals:
-        if i[0] <= x and x <= i[1]:
-            return True
-    return False
-
-def merge_intervals(x: tuple[int, int], y: tuple[int, int]):
+def merge_intervals(x: Interval|None, y: Interval):
+    if x == None: return y
+    if x[1] > y[1]: return x
     return (x[0], y[1])
 
+def are_overlaping(x: Interval|None, y: Interval) -> bool:
+    if x == None: return True
+    return x[1] >= y[0]
+
 def main():
-    with open("day05/python/test.txt", "r") as f:
-        intervals, values = list(map(lambda x: x.splitlines(), f.read().split("\n\n")))
+    with open("day05/python/input.txt", "r") as f:
+        intervals, _ = list(map(lambda x: x.splitlines(), f.read().split("\n\n")))
 
-    values = list(map(int, values))
-    intervals = list(map(parse_interval, intervals))
+    intervals = sorted(list(map(parse_interval, intervals)), key=lambda x: x[0])
+    current = None
+    result = 0 
+    for i in intervals:
+        if are_overlaping(current, i):
+            current = merge_intervals(current, i)
+        else:
+            result += (current[1] - current[0]) + 1
+            current = i
+    else:
+        result += (current[1] - current[0]) + 1
 
-    intervals = sorted(intervals, key=lambda x: x[0])
-
-
-    for interval in intervals:
-        to_merge = []
+    print(result)
 
 main()
 
+"""
+raw:
+3-5
+10-14
+16-20
+12-18
+
+sorted:
+3-5          ---
+10-14               ----   
+12-18                 ------               
+16-20                    ----------
+
+flatten:
+3-5, 10-20
+"""
